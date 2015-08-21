2bbbf50f53bbbd09567bb71e5d316292dd877b0a
Vykintas Narmontas <me@vynar.com>
2015-08-21 18:08:07 +0100

    Made the basic flow work - as previously with AngularJS

--- play/public/main.js
+++ play/public/main.js
@@ -1,4 +1,15 @@
+var currentLanguage;
+var currentCode;
+var currentCredit;
 function highlight(language, code, optionalCredit) {
+    var ci = $('#code-input');
+    if ( ci.val() != code ) {
+        ci.val(code);
+    }
+    if ( currentCode === code ) return;
+    currentLanguage = language;
+    currentCode = code;
+    currentCredit = optionalCredit;
     jsRoutes.controllers.Main.highlight(language).ajax({data: code,
         contentType: 'text/plain',
         success: function(rendered) {
@@ -7,20 +18,37 @@ function highlight(language, code, optionalCredit) {
             console.log("Faile dto highlight", e);
         }});
 }
-$("#dropdown-samples").find("a").click(function(event) {
-    var path = $(event.target).data("sample-path");
-    var language = $(event.target).data("sample-language");
-    var credit = $(event.target).data("sample-credit");
+function renderSample(a) {
+    var path = $(a).data("sample-path");
+    var language = $(a).data("sample-language");
+    var credit = $(a).data("sample-credit");
     $.ajax(path).then(function(data) {
-        console.log("Picked sample... ", data);
         highlight(language, data, credit);
     });
+}
+$("#dropdown-samples").find("a").click(function(event) {
+    renderSample(event.target);
 });
 $("#dropdown-styles").find("a").click(function(event) {
     selectStyle($(event.target).data("style-id"));
 });
-//highlight("scala", "import stuff._");
+$("#dropdown-languages").find("a").click(function(event) {
+    selectLanguage($(event.target).data("language-id"));
+});
+function selectLanguage(id) {
+    return highlight(id, currentCode, currentCredit);
+}
 function selectStyle(name) {
     $("#rendering-style").text($("*[data-style-id="+name+"]").data("style-css"));
 }
 selectStyle("monokai");
+
+$("#dropdown-samples").find("a[data-sample-language=scala]").first().each(function(_, a) { return renderSample(a); });
+$('#code-input').keyup(function(evt) {
+    var code = $(evt.target).val();
+    renderCode(code);
+});
+function renderCode(code) {
+    console.log("Render code = ", code);
+    highlight(currentLanguage, code, currentCredit);
+}
